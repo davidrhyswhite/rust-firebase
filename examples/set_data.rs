0ce9523b641246ec2e51beb1b60a01808b37a737
Michael Eden <themichaeleden@gmail.com>
2015-06-05 15:46:11 -0400

    examples fixes

--- examples/set_data.rs
+++ examples/set_data.rs
@@ -5,7 +5,7 @@ use firebase::Firebase;
 fn main() {
     let messages = Firebase::new("https://shining-torch-7752.firebaseio.com/api/messages.json");
 
-    let res = firebase.set("{\"name\":\"David White\",\"message\":\"Hello from Rust\"}");
+    let res = messages.set("{\"name\":\"David White\",\"message\":\"Hello from Rust\"}");
 
     println!("Response body: {:?}", res.body);
     println!("Response code: {:?}", res.code);
--- examples/update_data.rs
+++ examples/update_data.rs
@@ -5,13 +5,13 @@ use firebase::Firebase;
 fn main() {
     let david = Firebase::new("https://shining-torch-7752.firebaseio.com/users/david.json");
 
-    let res = firebase.set("{\"firstName\":\"Dave\"}");
+    let res = david.set("{\"firstName\":\"Dave\"}");
 
     println!("Response body: {:?}", res.body);
     println!("Response code: {:?}", res.code);
     println!("Response success: {:?}", res.is_success());
 
-    let res = firebase.update("{\"firstName\":\"David\"}");
+    let res = david.update("{\"firstName\":\"David\"}");
 
     println!("Response body: {:?}", res.body);
     println!("Response code: {:?}", res.code);
