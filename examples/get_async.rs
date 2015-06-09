0a6e3e5234d00681f543bfbb5238fa85b55a7fc9
Michael Eden <themichaeleden@gmail.com>
2015-06-09 09:31:02 -0400

    Added async example

---
+++ examples/get_async.rs
@@ -0,0 +1,18 @@
+extern crate firebase;
+
+use firebase::Firebase;
+
+fn main() {
+    let firebase = Firebase::new("https://shining-torch-7752.firebaseio.com");
+    let messages = firebase.at("/api/messages");
+
+    let job = messages.get_async(|result| {
+        let res = result.unwrap();
+        println!("Response body: {:?}", res.body);
+        println!("Response code: {:?}", res.code);
+        println!("Response success: {:?}", res.is_success());
+    });
+
+    println!("This will probably print first, because of async.");
+    job.join().ok();
+}
