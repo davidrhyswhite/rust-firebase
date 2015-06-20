1fd43ed84158359d5a2981a26f4842bc31daafc0
Michael Eden <themichaeleden@gmail.com>
2015-06-20 10:58:50 -0400

    Changed tests to all use HTTPS

--- tests/lib.rs
+++ tests/lib.rs
@@ -9,45 +9,45 @@ use std::sync::{Arc, Mutex};
 
 #[test]
 fn builds_auth_url() {
-    let f = Firebase::authed("http://db.rifebass.com/", "deadbeaf").ok().unwrap();
-    assert_eq!(f.get_url(), "http://db.rifebass.com/?auth=deadbeaf");
+    let f = Firebase::authed("https://db.rifebass.com/", "deadbeaf").ok().unwrap();
+    assert_eq!(f.get_url(), "https://db.rifebass.com/?auth=deadbeaf");
 }
 
 #[test]
 fn extends_auth_url() {
-    let f = Firebase::authed("http://db.rifebass.com/", "deadbeaf").ok().unwrap();
+    let f = Firebase::authed("https://db.rifebass.com/", "deadbeaf").ok().unwrap();
     let f = f.at("/futurama/SpacePilot3000").ok().unwrap();
-    let url_now = "http://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
+    let url_now = "https://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
     assert_eq!(url_now, f.get_url());
 }
 
 #[test]
 fn double_extends_url() {
-    let f = Firebase::authed("http://db.rifebass.com", "deadbeaf").ok().unwrap();
+    let f = Firebase::authed("https://db.rifebass.com", "deadbeaf").ok().unwrap();
     let f = f.at("/futurama.json").ok().unwrap();
     let f = f.at("SpacePilot3000").ok().unwrap();
-    let url_now = "http://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
+    let url_now = "https://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
     assert_eq!(url_now, f.get_url());
 }
 
 #[test]
 fn handle_slashes() {
-    let f = Firebase::authed("http://db.rifebass.com", "deadbeaf").ok().unwrap();
+    let f = Firebase::authed("https://db.rifebass.com", "deadbeaf").ok().unwrap();
     let f = f.at("futurama.json").ok().unwrap();
     let f = f.at("SpacePilot3000.json").ok().unwrap();
-    let url_now = "http://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
+    let url_now = "https://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
     assert_eq!(url_now, f.get_url());
 
-    let f = Firebase::authed("http://db.rifebass.com/", "deadbeaf").ok().unwrap();
+    let f = Firebase::authed("https://db.rifebass.com/", "deadbeaf").ok().unwrap();
     let f = f.at("/futurama/").ok().unwrap();
     let f = f.at("/SpacePilot3000/").ok().unwrap();
-    let url_now = "http://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
+    let url_now = "https://db.rifebass.com//futurama/SpacePilot3000.json?auth=deadbeaf";
     assert_eq!(url_now, f.get_url());
 }
 
 #[test]
 fn handle_json_suffix() {
-    let f = Firebase::new("http://db.rifebass.com").ok().unwrap();
+    let f = Firebase::new("https://db.rifebass.com").ok().unwrap();
     let f = f.at("0.json").ok().unwrap().at("1.json").ok().unwrap()
              .at("1.json").ok().unwrap().at("8.json").ok().unwrap()
              .at("9.json").ok().unwrap().at("9.json").ok().unwrap()
@@ -58,16 +58,16 @@ fn handle_json_suffix() {
              .at("1.json").ok().unwrap().at("9.json").ok().unwrap()
              .at("7.json").ok().unwrap().at("2.json").ok().unwrap()
              .at("5.json").ok().unwrap().at("3.json").ok().unwrap();
-    let url_now = "http://db.rifebass.com//0/1/1/8/9/9/9/8/8/1/9/9/9/1/1/9/7/2/5/3.json";
+    let url_now = "https://db.rifebass.com//0/1/1/8/9/9/9/8/8/1/9/9/9/1/1/9/7/2/5/3.json";
     assert_eq!(url_now, f.get_url());
 }
 
 #[test]
 fn test_ops() {
-    let f = Firebase::new("http://db.fe/").ok().expect("url err");
+    let f = Firebase::new("https://db.fe/").ok().expect("url err");
     let f = f.at("lol").ok().expect("extend err");
     let req = f.end_at(13).limit_to_first(4).equal_to(8).shallow(false);
-    let correct = Url::parse("http://db.fe//lol.json?limitToFirst=4&endAt=13&equalTo=8&shallow=false").ok().unwrap();
+    let correct = Url::parse("https://db.fe//lol.json?limitToFirst=4&endAt=13&equalTo=8&shallow=false").ok().unwrap();
     let generated = Url::parse(&req.get_url()).ok().unwrap();
 
     assert_queries(&correct, &generated);
@@ -75,7 +75,7 @@ fn test_ops() {
 
 #[test]
 fn test_auth_ops() {
-    let f = Firebase::authed("db.fe/", "key").ok().expect("url err").at("lol").ok().unwrap();
+    let f = Firebase::authed("https://db.fe/", "key").ok().expect("url err").at("lol").ok().unwrap();
     let req = f.order_by("pts").limit_to_last(5).start_at(8);
 
     let correct = Url::parse("https://db.fe/lol.json?auth=key&orderBy=pts&limitToLast=5&startAt=8").ok().unwrap();
@@ -86,7 +86,7 @@ fn test_auth_ops() {
 
 #[test]
 fn test_async_get() {
-    let fb = Firebase::new("mybd.firebase.com").ok().unwrap();
+    let fb = Firebase::new("https://mybd.firebase.com").ok().unwrap();
     let db_ref = fb.at("Profiles/a9sdc8asd99acc/profile_img").ok().unwrap();
 
     let finished = Arc::new(Mutex::new(false));
@@ -103,7 +103,7 @@ fn test_async_get() {
 
 #[test]
 fn test_ops_ctor() {
-    let fb = Firebase::new("db.fb.com").ok().unwrap();
+    let fb = Firebase::new("https://db.fb.com").ok().unwrap();
     let query = fb.ops(&FbOps {
         order_by:       Some("Hello World"),
         limit_to_first: Some(5),
